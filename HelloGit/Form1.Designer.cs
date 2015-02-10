0c337bbc5174468586f4a8a95cf8b2e07830fab3
andre.goncalves <andre.goncalves@supergrp.com>
2015-02-10 09:46:59 +0200

    Add hello world button

--- HelloGit/Form1.Designer.cs
+++ HelloGit/Form1.Designer.cs
@@ -29,6 +29,7 @@
         private void InitializeComponent()
         {
             this.button1 = new System.Windows.Forms.Button();
+            this.btnHelloWorld = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // button1
@@ -41,11 +42,21 @@
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
+            // btnHelloWorld
+            // 
+            this.btnHelloWorld.Location = new System.Drawing.Point(12, 12);
+            this.btnHelloWorld.Name = "btnHelloWorld";
+            this.btnHelloWorld.Size = new System.Drawing.Size(87, 31);
+            this.btnHelloWorld.TabIndex = 1;
+            this.btnHelloWorld.Text = "Hello World";
+            this.btnHelloWorld.UseVisualStyleBackColor = true;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(290, 275);
+            this.Controls.Add(this.btnHelloWorld);
             this.Controls.Add(this.button1);
             this.Name = "Form1";
             this.Text = "Form1";
@@ -56,6 +67,7 @@
         #endregion
 
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button btnHelloWorld;
     }
 }
 
