407fe38be69398fb0e9d28d3675e488d73ad1998
sleeper420za <martin.klasen@gmail.com>
2015-02-10 11:22:30 +0200

    Added even more buttons

--- HelloGit/Form1.Designer.cs
+++ HelloGit/Form1.Designer.cs
@@ -32,12 +32,13 @@
             this.btnHelloWorld = new System.Windows.Forms.Button();
             this.btnRandom = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // button1
             // 
             this.button1.Location = new System.Drawing.Point(396, 437);
-            this.button1.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
+            this.button1.Margin = new System.Windows.Forms.Padding(6);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(164, 73);
             this.button1.TabIndex = 0;
@@ -48,7 +49,7 @@
             // btnHelloWorld
             // 
             this.btnHelloWorld.Location = new System.Drawing.Point(24, 23);
-            this.btnHelloWorld.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
+            this.btnHelloWorld.Margin = new System.Windows.Forms.Padding(6);
             this.btnHelloWorld.Name = "btnHelloWorld";
             this.btnHelloWorld.Size = new System.Drawing.Size(174, 60);
             this.btnHelloWorld.TabIndex = 1;
@@ -58,7 +59,7 @@
             // btnRandom
             // 
             this.btnRandom.Location = new System.Drawing.Point(92, 246);
-            this.btnRandom.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
+            this.btnRandom.Margin = new System.Windows.Forms.Padding(6);
             this.btnRandom.Name = "btnRandom";
             this.btnRandom.Size = new System.Drawing.Size(150, 44);
             this.btnRandom.TabIndex = 2;
@@ -76,16 +77,26 @@
             this.button2.UseVisualStyleBackColor = true;
             this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(289, 156);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 146);
+            this.button3.TabIndex = 4;
+            this.button3.Text = "button3";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(664, 529);
+            this.Controls.Add(this.button3);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.btnRandom);
             this.Controls.Add(this.btnHelloWorld);
             this.Controls.Add(this.button1);
-            this.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
+            this.Margin = new System.Windows.Forms.Padding(6);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
@@ -98,6 +109,7 @@
         private System.Windows.Forms.Button btnHelloWorld;
         private System.Windows.Forms.Button btnRandom;
         private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
     }
 }
 
